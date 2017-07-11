6f65e5e98e37090e3be48f8a547936b4dc8f20fe
Erik Post <erik@shinsetsu.nl>
2017-07-11 14:22:28 +0200

    Initial commit.

---
+++ souffle-mode.el
@@ -0,0 +1,37 @@
+;;; souffle-mode.el --- Major mode for Souffle datalog files. -*- lexical-binding: t -*-
+
+;; Copyright (C) 2017 Erik Post
+
+;; Author   : Erik Post <erik@shinsetsu.nl>
+;; Homepage : https://github.com/epost/souffle-mode
+;; Version  : 0.1.0
+;; Keywords : languages
+
+;;; Commentary:
+
+;; Emacs integration for Souffle datalog files
+
+;;; Code:
+
+(defconst souffle-mode-syntax-table
+  (let ((table (make-syntax-table)))
+    (modify-syntax-entry ?/ "< 1" table)
+    (modify-syntax-entry ?/ "< 2" table)
+    (modify-syntax-entry ?\n "> " table)
+    table))
+
+(setq souffle-highlights
+      '((":-\\|=\\|:\\|\\[\\|\\]\\|\\.type\\|\\.decl\\|\\.comp\\|\\.init\\|\\." . font-lock-keyword-face)
+        ("\\number\\|symbol" . font-lock-builtin-face)
+        (":" . font-lock-constant-face)))
+
+(define-derived-mode souffle-mode fundamental-mode "souffle"
+  "major mode for editing Souffle datalog files."
+  :syntax-table souffle-mode-syntax-table
+  (setq font-lock-defaults '(souffle-highlights))
+
+  (setq-local comment-start "\/\/")
+  (setq-local comment-end "")
+)
+
+(provide 'souffle)
